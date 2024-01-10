9f248260a38e5a39b2d7e3d8a04275be1b00ae44
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2024-01-10 15:52:18 +0530

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import "./tasks/stake";
+import "./tasks/claim";
+import "./tasks/unstake";
+import "./tasks/beneficiary";
+import "./tasks/withdraw";
+import "./tasks/unstake";
+import "./tasks/address";
+import "./tasks/interact";
+import "./tasks/deploy";
+import "@nomicfoundation/hardhat-toolbox";
+import "@zetachain/toolkit/tasks";
+
+import { getHardhatConfigNetworks } from "@zetachain/networks";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  networks: {
+    ...getHardhatConfigNetworks(),
+  },
+  solidity: "0.8.7",
+};
+
+export default config;
